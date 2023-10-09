4e239cb64c3191abedc34a3c827ca22261e09476
atharvsurve <139199045+atharvsurve@users.noreply.github.com>
2023-10-10 00:01:28 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/CommonInterface.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Interface(),
+    ),
+  );
+}
